6a91937a3f91de5a2df7618a5445172d2c9fa08e
Muhammad Erlangga <muhammad.erlangga@trilogi.ac.id>
2024-07-17 08:58:18 +0700

    first commit

---
+++ components/atom/home/headerText.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { cn } from '@/lib/utils'
+
+export default function HeaderText({children, className} : {children: string, className?: string}) {
+  return (
+    <div>
+          <h1 className={cn('font-bold text-white', className)}>{ children }</h1>
+    </div>
+  )
+}
