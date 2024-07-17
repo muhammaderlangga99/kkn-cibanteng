462a70e9326b95b9445c7566407239da734a3b41
Muhammad Erlangga <muhammad.erlangga@trilogi.ac.id>
2024-07-17 11:43:23 +0700

    add: input search

--- components/atom/home/headerText.tsx
+++ components/atom/home/headerText.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { cn } from '@/lib/utils'
 
 export default function HeaderText({children, className} : {children: string, className?: string}) {
-  return (
-    <div>
-          <h1 className={cn('font-bold text-white', className)}>{ children }</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1 className={cn('font-bold text-white', className)}>{children}</h1>
+        </div>
+    );
 }
